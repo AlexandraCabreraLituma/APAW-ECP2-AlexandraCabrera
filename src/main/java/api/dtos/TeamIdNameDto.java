c68ebba740f31bedce6d909d017a8b39a574a148
alecabreralituma@gmail.com <alecabreralituma@gmail.com>
2018-10-16 23:11:13 +0200

    refactor issue #08

--- src/main/java/api/dtos/TeamIdNameDto.java
+++ src/main/java/api/dtos/TeamIdNameDto.java
@@ -14,14 +14,12 @@ public class TeamIdNameDto {
     public String getId() {
         return id;
     }
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
-
+    public void setId(String id) {
+        this.id = id;
+    }
     public void setName(String name) {
         this.name = name;
     }
